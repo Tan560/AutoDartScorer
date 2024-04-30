680ecffd239a25d8460363e5749a9534da425765
Tan560 <97492534+Tan560@users.noreply.github.com>
2024-04-30 16:16:31 -0400

    Update gameMode.py

--- src/gameMode.py
+++ src/gameMode.py
@@ -12,12 +12,21 @@ class gameMode301(gameMode):
         super().__init__()
         # Implement 301 game mode-specific logic here
 
+    def calculate_score(self, dart_position):
+        return super().calculate_score(dart_position)
+
 class gameMode501(gameMode):
     def __init__(self):
         super().__init__()
         # Implement 501 game mode-specific logic here
+    
+    def calculate_score(self, dart_position):
+        return super().calculate_score(dart_position)
 
 class gameModeCricket(gameMode):
     def __init__(self):
         super().__init__()
         # Implement Cricket game mode-specific logic here
+
+    def calculate_score(self, dart_position):
+        return super().calculate_score(dart_position)
