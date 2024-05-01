5a13c2a3c282dda6d62a8937d0814e4352205f99
Tan560 <97492534+Tan560@users.noreply.github.com>
2024-05-01 15:51:17 -0400

    All 3 game modes screens update

--- src/gameMode.py
+++ src/gameMode.py
@@ -76,6 +76,32 @@ class gameMode501(gameMode):
     def game_mode_screen(self):
         super().game_mode_screen()
 
+        running = True # Wait for user input before returning to the main screen (Main loop)
+        while running:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                elif event.type == MOUSEBUTTONDOWN and event.button == 1 and self.back_button_rect.collidepoint(event.pos):  # Left mouse button clicked
+                    running = False
+            
+            modeText = self.font.render("Game Mode: 501", True, (0, 255, 0))
+            modeText_rect = modeText.get_rect(center=(self.target_width / 2,50))
+
+            # Blit the text onto the screen
+            self.screen.blit(modeText, modeText_rect)
+            
+            # Update button color when mouse hovers over it
+            if self.back_button_rect.collidepoint(pygame.mouse.get_pos()):
+                button_color = (255, 25, 25)  # Darker color when mouse hovers over button
+            else:
+                button_color = (255, 0, 0)  # Default color
+                
+            # Draw button with updated color
+            pygame.draw.rect(self.screen, button_color, self.back_button_rect)
+            self.back_button_text_rect = self.back_button_text.get_rect(center=self.back_button_rect.center)
+            self.screen.blit(self.back_button_text, self.back_button_text_rect)
+            pygame.display.flip()
+
 class gameModeCricket(gameMode):
     def __init__(self):
         super().__init__()
@@ -85,3 +111,28 @@ class gameModeCricket(gameMode):
 
     def game_mode_screen(self):
         super().game_mode_screen()
+        running = True # Wait for user input before returning to the main screen (Main loop)
+        while running:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                elif event.type == MOUSEBUTTONDOWN and event.button == 1 and self.back_button_rect.collidepoint(event.pos):  # Left mouse button clicked
+                    running = False
+            
+            modeText = self.font.render("Game Mode: Cricket", True, (0, 255, 0))
+            modeText_rect = modeText.get_rect(center=(self.target_width / 2,50))
+
+            # Blit the text onto the screen
+            self.screen.blit(modeText, modeText_rect)
+            
+            # Update button color when mouse hovers over it
+            if self.back_button_rect.collidepoint(pygame.mouse.get_pos()):
+                button_color = (255, 25, 25)  # Darker color when mouse hovers over button
+            else:
+                button_color = (255, 0, 0)  # Default color
+                
+            # Draw button with updated color
+            pygame.draw.rect(self.screen, button_color, self.back_button_rect)
+            self.back_button_text_rect = self.back_button_text.get_rect(center=self.back_button_rect.center)
+            self.screen.blit(self.back_button_text, self.back_button_text_rect)
+            pygame.display.flip()
